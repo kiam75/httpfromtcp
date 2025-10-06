b89cfa0fc3727091863651e123d5e0162ad0f241
krausem <m.krause@eos-ts.com>
2025-10-06 07:31:52 +0200

    Finish buffer file read

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -9,10 +10,36 @@ import (
 func main() {
 
 	inputfile, err := os.Open("message.txt")
-	if err != nil{
-		log.Printf("Error open file. Errormessage: %v",err)
+	if err != nil {
+		log.Printf("Error open file. Errormessage: %v", err)
 	}
 	defer inputfile.Close()
 
-	
+	buffer := make([]byte, 8)
+
+	for {
+		n, err := inputfile.Read(buffer)
+
+		// Print the actuall bytes read (only n bytes, not the full buffer)
+		if n > 0 {
+			fmt.Printf("read: %s\n", buffer[:n])
+		}
+
+		// Handle errors with switch
+		switch err {
+		case nil:
+			// Continue reading
+			continue
+		case io.EOF:
+			// End of file reached, exit cleanly
+			//break
+			os.Exit(0)
+		default:
+			// Handle other errors
+			log.Printf("Error reading file: %v", err)
+			//break
+			os.Exit(1)
+		}
+	}
+
 }
